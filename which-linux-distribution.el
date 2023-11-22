8e8639d3d262379dbcf96fba058f861f388b3217
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2023-11-22 13:53:41 +0100

    Fix formatting and command

--- which-linux-distribution.el
+++ which-linux-distribution.el
@@ -1,7 +1,15 @@
+;;; which-linux-distribution --- Get linux distro form lsb_release -sd
+;;
+;;; Commentary:
 ;; from https://emacs.stackexchange.com/questions/18205/how-can-i-distinguish-between-linux-distributions-in-emacs
+;; Gets the distro name using lsb_release -sd, throws an error if it isn't found
+
+;;; Code:
 
 (defun which-linux-distribution ()
-  "Get linux distro from uname -n"
-  (interactive)
-  (when (eq system-type 'gnu/linux)
-	(shell-command-to-string "uname -n")))
+  "Get Linux distro using lsb_release."
+  (if (executable-find "lsb_release")
+      (shell-command-to-string "lsb_release -d -s")
+    (error "Program lsb_release command not found.  Unable to determine Linux distribution")))
+
+;;; which-linux-distribution.el ends here
